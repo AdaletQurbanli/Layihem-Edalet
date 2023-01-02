8342c48243336ce57c0d4c3c71acb099e730da91
AdaletQurbanli <115143679+AdaletQurbanli@users.noreply.github.com>
2023-01-02 20:02:41 +0400

    Add files via upload
    Layihe tapsirigmin helli

---
+++ edalet/js/webJS.js
@@ -0,0 +1,106 @@
+function kecid(){
+        document.getElementById("sehife").style.display="block";
+        document.getElementById("qara").style.display="block";
+}
+function bagla(){
+     document.getElementById("sehife").style.display="none";
+     document.getElementById("qara").style.display="none";
+}
+$(document).ready(function(){
+        //k1 nin hover effecti
+      $("#k1").hover(function(){
+        $("#k1").css({"background-color":"#0C0404","color":"#f95911"});
+        $("#ks1").css("color","#f95911"); 
+        $("#ka1").css({"color":"#f95911","border-color":"#f95911"}); 
+        $("#k1").css("margin-top","-5px"); 
+        $("#k1").css("box-shadow","-1px -1px 20px 3px black");
+},
+      function(){
+        $("#k1").css({"background-color":"#ffffff","color":"black"});
+        $("#ks1").css("color","gray"); 
+        $("#ka1").css("color","black");
+        $("#k1").css("margin-top","0px");  
+        $("#k1").css("box-shadow","none");
+      });
+
+      //k2 nin hover effecti
+      $("#k2").hover(function(){
+        $("#k2").css({"background-color":"#0C0404","color":"#f95911"});
+        $("#ks2").css("color","#f95911"); 
+        $("#ka2").css({"color":"#f95911","border-color":"#f95911"}); 
+        $("#k2").css("margin-top","-5px");   
+        $("#k2").css("box-shadow","-1px -1px 20px 3px black");   
+},
+      function(){
+        $("#k2").css({"background-color":"#ffffff","color":"black"});
+        $("#ks2").css("color","gray"); 
+        $("#ka2").css("color","black"); 
+        $("#k2").css("margin-top","0px");  
+        $("#k2").css("box-shadow","none");
+      });
+
+      //k3 nin hover effecti
+      $("#k3").hover(function(){
+        $("#k3").css({"background-color":"#0C0404","color":"#f95911"});
+        $("#ks3").css("color","#f95911"); 
+        $("#ka3").css({"color":"#f95911","border-color":"#f95911"});   
+        $("#k3").css("margin-top","-5px");  
+        $("#k3").css("box-shadow","-1px -1px 20px 3px black");  
+},
+      function(){
+        $("#k3").css({"background-color":"#ffffff","color":"black"});
+        $("#ks3").css("color","gray"); 
+        $("#ka3").css("color","black"); 
+        $("#k3").css("margin-top","0px");  
+        $("#k3").css("box-shadow","none");
+      });
+
+      //k4 nin hover effecti
+      $("#k4").hover(function(){
+        $("#k4").css({"background-color":"#0C0404","color":"#f95911"});
+        $("#ks4").css("color","#f95911"); 
+        $("#ka4").css({"color":"#f95911","border-color":"#f95911"});     
+        $("#k4").css("margin-top","-5px");  
+        $("#k4").css("box-shadow","-1px -1px 20px 3px black");
+},
+      function(){
+        $("#k4").css({"background-color":"#ffffff","color":"black"});
+        $("#ks4").css("color","gray"); 
+        $("#ka4").css("color","black"); 
+        $("#k4").css("margin-top","0px");  
+        $("#k4").css("box-shadow","none");
+      });
+
+      //k5 nin hover effecti
+      $("#k5").hover(function(){
+        $("#k5").css({"background-color":"#0C0404","color":"#f95911"});
+        $("#ks5").css("color","#f95911"); 
+        $("#ka5").css({"color":"#f95911","border-color":"#f95911"});    
+        $("#k5").css("margin-top","-5px"); 
+        $("#k5").css("box-shadow","-1px -1px 20px 3px black");  
+},
+      function(){
+        $("#k5").css({"background-color":"#ffffff","color":"black"});
+        $("#ks5").css("color","gray"); 
+        $("#ka5").css("color","black"); 
+        $("#k5").css("margin-top","0px");  
+        $("#k5").css("box-shadow","none");
+      });
+
+      //k6 nin hover effecti
+      $("#k6").hover(function(){
+        $("#k6").css({"background-color":"#0C0404","color":"#f95911"});
+        $("#ks6").css("color","#f95911"); 
+        $("#ka6").css({"color":"#f95911","border-color":"#f95911"});  
+        $("#k6").css("margin-top","-5px");  
+        $("#k6").css("box-shadow","-1px -1px 20px 3px black");   
+},
+      function(){
+        $("#k6").css({"background-color":"#ffffff","color":"black"});
+        $("#ks6").css("color","gray"); 
+        $("#ka6").css("color","black"); 
+        $("#k6").css("margin-top","0px");
+        $("#k6").css("box-shadow","none");  
+      });
+});
+
